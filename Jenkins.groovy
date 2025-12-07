8f21f15cd3d9f8bd4140ffbc08dc28a2837e83ca
Nguyen Chi Tao <nctao.vcbs@vcbs.com>
2025-12-07 16:28:41 +0700

    test

--- Jenkins.groovy
+++ Jenkins.groovy
@@ -1,16 +1,9 @@
-podTemplate(containers: [
-    containerTemplate(
-        name: 'maven',
-        image: 'maven:3.9.6-eclipse-temurin-17', 
-        command: 'sleep', 
-        ttyEnabled: 'true',
-        args: '30d'
-        ),
-    containerTemplate(
-        name: 'python', 
-        image: 'python:latest', 
-        command: 'sleep', 
-        args: '30d')
+podTemplate(
+    agentContainer: 'maven',
+    agentInjection: true,
+    containers: [
+        containerTemplate(name: 'maven', image: 'maven:3.9.9-eclipse-temurin-17'),
+        containerTemplate(name: 'golang', image: 'golang:1.16.5', command: 'sleep', args: '99d')
   ]) {
     node('kubeagent') {
         stage('Get a Maven project') {
